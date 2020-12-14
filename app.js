c3306de20fd4d4502aac15021a3d8f395128865e
Priyanshi Agrawal <priyanshiagrawalccs@gmail.com>
2020-12-13 20:10:01 -0800

    parsed request body

--- app.js
+++ app.js
@@ -10,13 +10,27 @@ const server = http.createServer((req,res) => {
     if(req.url === '/'){
         res.write(`<html><head><title>Priyanshi</title></head>
         <body><form action = '/message' method = 'POST'>
-        <input type='text' name = 'message'>
+        <input type='text' name = 'name'>
+        <input type='email' name = 'email'>
+
         <button type='submit'>submit</button></form></body>
         </html>`)
         return res.end()
     };
     if(req.url === '/message' && req.method == "POST"){
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body =[];
+        // req.on() helps to listen to certain events.
+        req.on('data', (chunk) => {
+            body.push(chunk);
+            console.log(chunk)
+        })
+        req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('&')[1]
+            fs.writeFileSync('message.txt', message);
+
+        })
+        fs.writeFileSync('message.txt', 'DUMMY data');
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end()
